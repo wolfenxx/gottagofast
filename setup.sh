1397f48706dcdd7a44f62d793bdd912656e0f451
Carlos Salazar <csalazar@hey.com>
2024-02-09 08:36:03 -0600

    add nodejs to setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ apt install curl -y
 apt install tmux -y
 apt install git -y
 apt install python3 -y
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+apt install -y nodejs
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
@@ -13,4 +15,5 @@ install lazygit /usr/local/bin
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
+mv squashfs-root /
 ln -s /squashfs-root/AppRun /usr/bin/nvim
