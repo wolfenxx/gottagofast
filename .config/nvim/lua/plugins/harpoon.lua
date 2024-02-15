19269f0b2706b97f263c582d47743eae7d5ae97e
Carlos Salazar <csalazar@hey.com>
2024-02-15 10:23:25 -0600

    update harpoon keymaps

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -3,16 +3,21 @@ return {
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		local harpoon = require("harpoon").setup()
+		local harpoon = require("harpoon"):setup()
 
 		vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
 		-- commented out to use with telescope instead
 		--vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 		vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-		vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-		vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-		vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+		vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+		vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
+		vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+
+		vim.keymap.set("n", "<leader>hh", function() harpoon:list():removeAt(1) end)
+		vim.keymap.set("n", "<leader>jj", function() harpoon:list():removeAt(2) end)
+		vim.keymap.set("n", "<leader>kk", function() harpoon:list():removeAt(3) end)
+		vim.keymap.set("n", "<leader>ll", function() harpoon:list():removeAt(4) end)
 
 		-- Toggle previous & next buffers stored within Harpoon list
 		vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
