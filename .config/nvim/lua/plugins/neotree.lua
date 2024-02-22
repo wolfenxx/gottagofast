9e20f13ad0a678718d961e694c833918da5b32a1
Carlos Salazar <csalazar@hey.com>
2024-02-22 14:33:34 -0600

    neotree show hidden files

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -8,6 +8,20 @@ return {
 	},
 	config = function()
 		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					show_hidden_count = true,
+					hide_dotfiles = false,
+					hide_gitignored = true,
+					hide_by_name = {
+						-- '.git',
+						-- '.DS_Store',
+						-- 'thumbs.db',
+					},
+				},
+				never_show = {},
+			},
 			event_handlers = {
 				{
 					event = "file_opened",
