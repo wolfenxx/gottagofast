451c07617ca74c30bdf30e030415c927a4047097
Carlos Salazar <csalazar@hey.com>
2024-02-22 14:27:12 -0600

    vimsurround plug and neotree config updates

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -7,6 +7,16 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
+		require("neo-tree").setup({
+			event_handlers = {
+				{
+					event = "file_opened",
+					handler = function()
+						require("neo-tree.command").execute({ action = "close" })
+					end
+				},
+			}
+		})
 		vim.keymap.set('n', '<leader>fs', ':Neotree filesystem reveal left<CR>', {})
 	end
 }
