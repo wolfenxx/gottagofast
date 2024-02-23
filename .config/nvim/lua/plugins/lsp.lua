e369cc5febc791d15d81015ad2ca0782515b7a21
Carlos Salazar <csalazar@hey.com>
2024-02-22 18:56:59 -0600

    update tele and lsp configs

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = {"lua_ls", "tsserver", "pyright"}
+				ensure_installed = { "lua_ls", "tsserver", "pyright" }
 			})
 		end
 	},
@@ -21,9 +21,18 @@ return {
 			lspconfig.tsserver.setup({})
 			lspconfig.pyright.setup({})
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
 			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-			vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+			vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+			vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, {})
+			vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, {})
+			vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
+			vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
+			vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
+			vim.keymap.set('n', '<space>f', function()
+				vim.lsp.buf.format { async = true }
+			end, {})
 		end
 	},
 	{
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -15,6 +15,16 @@ return {
 		'nvim-telescope/telescope-ui-select.nvim',
 		config = function()
 			require("telescope").setup({
+				defaults = {
+					file_ignore_patterns = {
+						"node_modules", ".git"
+					}
+				},
+				pickers = {
+					find_files= {
+						hidden = true,
+					},
+				},
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown {
