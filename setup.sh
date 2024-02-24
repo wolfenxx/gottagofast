3862fd28e7702be222ceb1090f882ba21d8f172c
Carlos Salazar <csalazar@hey.com>
2024-02-23 20:30:01 -0600

    include mono runtime in setup.sh

--- setup.sh
+++ setup.sh
@@ -44,6 +44,13 @@ apt install aspnetcore-runtime-8.0 -y
 echo "Installing dotnet runtime..."
 apt install dotnet-runtime-8.0 -y
 
+echo "Installing mono runtime..."
+apt install ca-certificates gnupg
+gpg --homedir /tmp --no-default-keyring --keyring /usr/share/keyrings/mono-official-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb [signed-by=/usr/share/keyrings/mono-official-archive-keyring.gpg] https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+apt update
+apt install mono-devel
+
 echo "Installing nodejs..."
 curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
 apt install -y nodejs
