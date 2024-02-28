04cbbc9761ef6f9f072e3cc6c575f03c11715172
Carlos Salazar <csalazar@hey.com>
2024-02-28 11:56:44 -0600

    customize lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -4,7 +4,21 @@ return {
 		require('lualine').setup({
 			options = {
 				theme = 'dracula'
-			}
+			},
+			sections = {
+				lualine_a = {'mode'},
+				lualine_b = {},
+				lualine_c = {
+					{
+						'filename',
+						file_status = false,
+						path = 1
+					}
+				},
+				lualine_x = {},
+				lualine_y = {'progress'},
+				lualine_z = {'location'}
+			},
 		})
 	end
 }
