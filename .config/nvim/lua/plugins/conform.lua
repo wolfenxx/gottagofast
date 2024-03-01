c7875cba019e93bd0570b581d6be4dc2b8ec37b7
Carlos Salazar <csalazar@hey.com>
2024-03-01 12:59:00 -0600

    add formatters, clean up autocmds

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,31 @@
+return {
+	"stevearc/conform.nvim",
+	-- event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- Conform will run multiple formatters sequentially
+				python = { "isort", "black" },
+				-- Use a sub-list to run only the first available formatter
+				javascript = { { "prettierd", "prettier" } },
+				typescript = { { "prettierd", "prettier" } },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>fm", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end)
+	end,
+}
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -9,7 +9,11 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver", "pyright" },
+				ensure_installed = {
+					"lua_ls",
+					"tsserver",
+					"pyright",
+				},
 			})
 		end,
 	},
@@ -36,35 +40,94 @@ return {
 		end,
 	},
 	{
-		"nvimtools/none-ls.nvim",
-		config = function()
-			local null_ls = require("null-ls")
-
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.prettier,
-					null_ls.builtins.completion.spell,
-				},
-			})
-
-			vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
-		end,
-	},
-	{
-		"jay-babu/mason-null-ls.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"williamboman/mason.nvim",
-			"nvimtools/none-ls.nvim",
-		},
-		config = function()
-			require("mason-null-ls").setup({
-				ensure_installed = { "stylua", "prettier" },
-			})
-		end,
-	},
-	{
 		"OmniSharp/omnisharp-vim",
+		config = function()
+			vim.api.nvim_create_augroup("omnisharp", { clear = true })
+			vim.api.nvim_create_autocmd("FileType", {
+				group = "omnisharp",
+				command = "nmap <silent> <buffer> <Leader>osgd <Plug>(omnisharp_go_to_definition)",
+				pattern = "cs",
+			})
+			vim.api.nvim_create_autocmd("FileType", {
+				group = "omnisharp",
+				command = "nmap <silent> <buffer> <Leader>osfu <Plug>(omnisharp_find_usages)",
+				pattern = "cs",
+			})
+			vim.api.nvim_create_autocmd("FileType", {
+				group = "omnisharp",
+				command = "nmap <silent> <buffer> <Leader>osfi <Plug>(omnisharp_find_implementations)",
+				pattern = "cs",
+			})
+			vim.api.nvim_create_autocmd("FileType", {
+				group = "omnisharp",
+				command = "nmap <silent> <buffer> <Leader>ospd <Plug>(omnisharp_preview_definition)",
+				pattern = "cs",
+			})
+			vim.api.nvim_create_autocmd("FileType", {
+				group = "omnisharp",
+				command = "nmap <silent> <buffer> <Leader>ospi <Plug>(omnisharp_preview_implementations)",
+				pattern = "cs",
+			})
+			vim.api.nvim_create_autocmd("FileType", {
+				group = "omnisharp",
+				command = "nmap <silent> <buffer> <Leader>ostl <Plug>(omnisharp_type_lookup)",
+				pattern = "cs",
+			})
+			vim.api.nvim_create_autocmd("FileType", {
+				group = "omnisharp",
+				command = "nmap <silent> <buffer> <Leader>osdc <Plug>(omnisharp_documentation)",
+				pattern = "cs",
+			})
+			vim.api.nvim_create_autocmd("FileType", {
+				group = "omnisharp",
+				command = "nmap <silent> <buffer> <Leader>osfs <Plug>(omnisharp_find_symbol)",
+				pattern = "cs",
+			})
+			vim.api.nvim_create_autocmd("FileType", {
+				group = "omnisharp",
+				command = "nmap <silent> <buffer> <Leader>osfx <Plug>(omnisharp_fix_usings)",
+				pattern = "cs",
+			})
+
+			-- Find all code errors/warnings for the current solution and populate the quickfix window
+			vim.api.nvim_create_autocmd("FileType", {
+				group = "omnisharp",
+				command = "nmap <silent> <buffer> <Leader>osgcc <Plug>(omnisharp_global_code_check)",
+				pattern = "cs",
+			})
+
+			-- Contextual code actions (uses fzf, vim-clap, CtrlP or unite.vim selector when available)
+			vim.api.nvim_create_autocmd("FileType", {
+				group = "omnisharp",
+				command = "nmap <silent> <buffer> <Leader>osca <Plug>(omnisharp_code_actions)",
+				pattern = "cs",
+			})
+
+			vim.api.nvim_create_autocmd("FileType", {
+				group = "omnisharp",
+				command = "nmap <silent> <buffer> <Leader>os= <Plug>(omnisharp_code_format)",
+				pattern = "cs",
+			})
+			vim.api.nvim_create_autocmd("FileType", {
+				group = "omnisharp",
+				command = "nmap <silent> <buffer> <Leader>osnm <Plug>(omnisharp_rename)",
+				pattern = "cs",
+			})
+			vim.api.nvim_create_autocmd("FileType", {
+				group = "omnisharp",
+				command = "nmap <silent> <buffer> <Leader>osre <Plug>(omnisharp_restart_server)",
+				pattern = "cs",
+			})
+			vim.api.nvim_create_autocmd("FileType", {
+				group = "omnisharp",
+				command = "nmap <silent> <buffer> <Leader>osst <Plug>(omnisharp_start_server)",
+				pattern = "cs",
+			})
+			vim.api.nvim_create_autocmd("FileType", {
+				group = "omnisharp",
+				command = "nmap <silent> <buffer> <Leader>ossp <Plug>(omnisharp_stop_server)",
+				pattern = "cs",
+			})
+		end,
 	},
 }
