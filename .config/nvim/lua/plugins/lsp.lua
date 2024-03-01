18829b72bc848d5f10e428812a7add61e550d27d
Carlos Salazar <csalazar@hey.com>
2024-03-01 10:54:27 -0600

    update nonels with masonlsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,15 +3,15 @@ return {
 		"williamboman/mason.nvim",
 		config = function()
 			require("mason").setup()
-		end
+		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver", "pyright" }
+				ensure_installed = { "lua_ls", "tsserver", "pyright" },
 			})
-		end
+		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
@@ -21,21 +21,50 @@ return {
 			lspconfig.tsserver.setup({})
 			lspconfig.pyright.setup({})
 
-			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
-			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-			vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-			vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, {})
-			vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, {})
-			vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
-			vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
-			vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
-			vim.keymap.set('n', '<space>f', function()
-				vim.lsp.buf.format { async = true }
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {})
+			vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, {})
+			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
+			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<space>f", function()
+				vim.lsp.buf.format({ async = true })
 			end, {})
-		end
+		end,
 	},
 	{
-		"OmniSharp/omnisharp-vim"
-	}
+		"nvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.prettier,
+					null_ls.builtins.completion.spell,
+				},
+			})
+
+			vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
+		end,
+	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			require("mason-null-ls").setup({
+				ensure_installed = { "stylua", "prettier" },
+			})
+		end,
+	},
+	{
+		"OmniSharp/omnisharp-vim",
+	},
 }
