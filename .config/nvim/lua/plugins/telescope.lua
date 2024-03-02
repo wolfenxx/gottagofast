4475199f4d8f7b3a5a4d695948dfd2fa9a0482c5
Carlos Salazar <csalazar@hey.com>
2024-03-01 19:30:37 -0600

    add dressing plugin and telescope movement bindings

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,58 +1,73 @@
 return {
 	{
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.5',
-		dependencies = { 'nvim-lua/plenary.nvim' },
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
-			local builtin = require('telescope.builtin')
-			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-		end
+			local telescope = require("telescope")
+			local actions = require("telescope.actions")
+			local builtin = require("telescope.builtin")
+
+			telescope.setup({
+				defaults = {
+					mappings = {
+						i = {
+							["<C-k>"] = actions.move_selection_previous,
+							["<C-j>"] = actions.move_selection_next,
+							["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						},
+					},
+				},
+			})
+
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+		end,
 	},
 	{
-		'nvim-telescope/telescope-ui-select.nvim',
+		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
 			require("telescope").setup({
 				defaults = {
 					file_ignore_patterns = {
-						"node_modules", ".git"
-					}
+						"node_modules",
+						".git",
+					},
 				},
 				pickers = {
-					find_files= {
+					find_files = {
 						hidden = true,
 					},
 				},
 				extensions = {
 					["ui-select"] = {
-						require("telescope.themes").get_dropdown {
-						}
-					}
-				}
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
 			})
 			require("telescope").load_extension("ui-select")
-		end
+		end,
 	},
 	{
-		'nvim-telescope/telescope-fzf-native.nvim',
-		build = 'make',
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
 		config = function()
-			require('telescope').setup {
+			require("telescope").setup({
 				extensions = {
 					fzf = {
-						fuzzy = true,                    -- false will only do exact matching
-						override_generic_sorter = true,  -- override the generic sorter
-						override_file_sorter = true,     -- override the file sorter
-						case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+						fuzzy = true, -- false will only do exact matching
+						override_generic_sorter = true, -- override the generic sorter
+						override_file_sorter = true, -- override the file sorter
+						case_mode = "smart_case", -- or "ignore_case" or "respect_case"
 						-- the default case_mode is "smart_case"
-					}
-				}
-			}
+					},
+				},
+			})
 			-- To get fzf loaded and working with telescope, you need to call
 			-- load_extension, somewhere after setup function:
-			require('telescope').load_extension('fzf')
-		end
-	}
+			require("telescope").load_extension("fzf")
+		end,
+	},
 }
