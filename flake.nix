5e7ec7aff7f6a37079fb593dc5c69d29cca83ea8
Carlos Salazar <csalazar@hey.com>
2024-07-27 14:37:06 -0500

    fix

--- flake.nix
+++ flake.nix
@@ -69,7 +69,6 @@
           install = pkgs.writeShellApplication {
             name = "install";
             runtimeInputs = with pkgs; [ git ]; # I could make this fancier by adding other deps
-	    #text = builtins.readFile ./configure.sh;
             text = ''${./install.sh} "$@"'';
           };
         });
