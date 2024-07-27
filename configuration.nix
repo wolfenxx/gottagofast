b8ad2a3f7771b097eff7d23700114d4d11685159
Carlos Salazar <csalazar@hey.com>
2024-07-27 11:47:29 -0500

    fix semicolon

--- configuration.nix
+++ configuration.nix
@@ -76,7 +76,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${userSettings.username} = {
     isNormalUser = true;
-    description = userSettings.name;
+    description = userSettings.username;
     extraGroups = [ "networkmanager" "wheel" "docker" ];
     packages = with pkgs; [
     #  thunderbird
