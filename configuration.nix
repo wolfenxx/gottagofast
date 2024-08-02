d2a883eca497be6eca0bfb065a3b71c0ea1eee78
Carlos Salazar <csalazar@hey.com>
2024-08-01 19:46:35 -0500

    nvidia support

--- configuration.nix
+++ configuration.nix
@@ -23,6 +23,21 @@
   programs.hyprland.enable = true;
   # programs.hyprland.package = inputs.hyprland.packages."${systemSettings.system}".hyprland;
 
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+	hardware = {
+	  opengl.enable = true;
+		nvidia = {
+      modesetting.enable = true;
+      powerManagement.enable = false;
+      powerManagement.finegrained = false;
+      open = false;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+	};
+
   # Set your time zone.
   time.timeZone = systemSettings.timezone;
 
