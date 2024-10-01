de420e0dce8f6ad6219e7ade51c6da4fde624104
Carlos Salazar <csalazar@hey.com>
2024-10-01 10:34:00 -0500

    add fold plugin

---
+++ .config/nvim/lua/plugins/nvimufo.lua
@@ -0,0 +1,16 @@
+return {
+	"kevinhwang91/nvim-ufo",
+	dependencies = { "kevinhwang91/promise-async" },
+	config = function()
+		local ufo = require("ufo")
+
+		vim.keymap.set("n", "zR", ufo.openAllFolds)
+		vim.keymap.set("n", "zM", ufo.closeAllFolds)
+
+		ufo.setup({
+			provider_selector = function(bufnr, filetype, buftype)
+				return { "lsp", "treesitter" }
+			end,
+		})
+	end,
+}
--- .config/nvim/lua/vim-config.lua
+++ .config/nvim/lua/vim-config.lua
@@ -7,3 +7,7 @@ vim.cmd("set clipboard=unnamedplus")
 vim.keymap.set("n", "<Leader>qt", ":q!<CR>", {})
 vim.keymap.set("n", "<Leader>sq", ":wq<CR>", {})
 vim.g.OmniSharp_server_path = "/home/wolfen/omnisharp/bin/OmniSharp" --NIXOS
+vim.o.foldcolumn = "1"
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
