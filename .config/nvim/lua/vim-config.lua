7e63a774a383ea8a6022540ffaa1bede482bcf4e
Carlos Salazar <csalazar@hey.com>
2024-10-01 09:51:37 -0500

    add code folding options

--- .config/nvim/lua/vim-config.lua
+++ .config/nvim/lua/vim-config.lua
@@ -7,3 +7,9 @@ vim.cmd("set clipboard=unnamedplus")
 vim.keymap.set("n", "<Leader>qt", ":q!<CR>", {})
 vim.keymap.set("n", "<Leader>sq", ":wq<CR>", {})
 vim.g.OmniSharp_server_path = "/home/wolfen/omnisharp/bin/OmniSharp" --NIXOS
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldcolumn = "1"
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 1
+vim.opt.foldnestmax = 20
