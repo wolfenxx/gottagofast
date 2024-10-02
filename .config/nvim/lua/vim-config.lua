b52018e019d8e98a7eb58e977ea346336f7db5fc
Carlos Salazar <csalazar@hey.com>
2024-10-02 10:43:39 -0500

    line swap shortcuts

--- .config/nvim/lua/vim-config.lua
+++ .config/nvim/lua/vim-config.lua
@@ -6,6 +6,10 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set clipboard=unnamedplus")
 vim.keymap.set("n", "<Leader>qt", ":q!<CR>", {})
 vim.keymap.set("n", "<Leader>sq", ":wq<CR>", {})
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==") -- move line up(n)
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==") -- move line down(n)
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv") -- move line down(v)
 vim.g.OmniSharp_server_path = "/home/wolfen/omnisharp/bin/OmniSharp" --NIXOS
 vim.o.foldcolumn = "1"
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
