b373a651fcfebc5a5149a318806bbf5d7e06e88b
Carlos Salazar <csalazar@hey.com>
2024-12-03 17:57:30 -0600

    test

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -57,11 +57,11 @@ return {
 			},
 		})
 
-		cmp.setup.cmdline(":", {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = cmp.config.sources({
-				{ name = "path" },
-			}),
-		})
+		-- cmp.setup.cmdline(":", {
+		-- 	mapping = cmp.mapping.preset.cmdline(),
+		-- 	sources = cmp.config.sources({
+		-- 		{ name = "path" },
+		-- 	}),
+		-- })
 	end,
 }
