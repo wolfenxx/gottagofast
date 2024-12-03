489f32bf0832d980ce352d796c77f9dbe78d8856
Carlos Salazar <csalazar@hey.com>
2024-12-02 20:33:53 -0600

    dbui toggle shortcut

--- .config/nvim/lua/plugins/db.lua
+++ .config/nvim/lua/plugins/db.lua
@@ -4,6 +4,9 @@ return {
 	},
 	{
 		"kristijanhusak/vim-dadbod-ui",
+		config = function()
+			vim.keymap.set("n", "<Leader>db", "<cmd>DBUIToggle<CR>", {})
+		end,
 	},
 	{
 		"kristijanhusak/vim-dadbod-completion",
