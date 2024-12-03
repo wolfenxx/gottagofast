72c128898fd0b1b008805ebd6fd658a7a58ed15a
Carlos Salazar <csalazar@hey.com>
2024-12-02 20:01:00 -0600

    vim dadbod completion

--- .config/nvim/lua/plugins/db.lua
+++ .config/nvim/lua/plugins/db.lua
@@ -1,14 +1,11 @@
 return {
 	{
 		"tpope/vim-dadbod",
-		config = function()
-			-- vim.keymap.set("n", "<Leader>dpr", require("dap-python").test_method, {})
-		end,
 	},
 	{
 		"kristijanhusak/vim-dadbod-ui",
-		config = function()
-			-- vim.keymap.set("n", "<Leader>dpr", require("dap-python").test_method, {})
-		end,
+	},
+	{
+		"kristijanhusak/vim-dadbod-completion",
 	},
 }
