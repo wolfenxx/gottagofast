4d2b03cfb49a7a9e635728413dd9ad4c1fb005d8
Carlos Salazar <csalazar@hey.com>
2024-12-02 19:50:22 -0600

    add db plugins to nvim

---
+++ .config/nvim/lua/plugins/db.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"tpope/vim-dadbod",
+		config = function()
+			-- vim.keymap.set("n", "<Leader>dpr", require("dap-python").test_method, {})
+		end,
+	},
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		config = function()
+			-- vim.keymap.set("n", "<Leader>dpr", require("dap-python").test_method, {})
+		end,
+	},
+}
