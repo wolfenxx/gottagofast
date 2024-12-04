1995d75376e339ef14e621c3fea90f2f98a209c1
Carlos Salazar <csalazar@hey.com>
2024-12-03 18:18:32 -0600

    fix

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,9 +1,9 @@
 return {
 	"folke/noice.nvim",
-	event = "VeryLazy",
-	opts = {
-		-- add any options here
-	},
+	-- event = "VeryLazy",
+	-- opts = {
+	-- 	-- add any options here
+	-- },
 	dependencies = {
 		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 		"MunifTanjim/nui.nvim",
