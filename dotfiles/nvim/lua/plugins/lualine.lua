c312cec0d8764647a31909ace87c54f02f5f7195
Carlos Salazar <csalazar@hey.com>
2024-12-19 20:27:18 -0600

    leap.nvim plugin

--- dotfiles/nvim/lua/plugins/lualine.lua
+++ dotfiles/nvim/lua/plugins/lualine.lua
@@ -1,23 +1,23 @@
 return {
-	'nvim-lualine/lualine.nvim',
+	"nvim-lualine/lualine.nvim",
 	config = function()
-		require('lualine').setup({
+		require("lualine").setup({
 			options = {
-				theme = 'dracula'
+				theme = "dracula"
 			},
 			sections = {
-				lualine_a = {'mode'},
+				lualine_a = { "mode" },
 				lualine_b = {},
 				lualine_c = {
 					{
-						'filename',
+						"filename",
 						file_status = false,
 						path = 1
 					}
 				},
 				lualine_x = {},
-				lualine_y = {'progress'},
-				lualine_z = {'location'}
+				lualine_y = { "progress" },
+				lualine_z = { "location" }
 			},
 		})
 	end
