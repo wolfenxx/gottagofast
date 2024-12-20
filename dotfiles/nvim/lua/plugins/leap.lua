0b586e2067c9ff3d58049f12731d6916b79e6a16
Carlos Salazar <csalazar@hey.com>
2024-12-19 20:38:12 -0600

    add some deps

--- dotfiles/nvim/lua/plugins/leap.lua
+++ dotfiles/nvim/lua/plugins/leap.lua
@@ -1,5 +1,6 @@
 return {
 	"ggandor/leap.nvim",
+	dependencies = { "tpope/vim-repeat" },
 	config = function()
 		vim.keymap.set({ "n", "x", "o" }, "<leader>s", "<Plug>(leap-forward)")
 		vim.keymap.set({ "n", "x", "o" }, "<leader>S", "<Plug>(leap-backward)")
