b446ec5f9b3ec4799d21c12fb52dfa45a07d4374
Carlos Salazar <csalazar@hey.com>
2024-12-23 11:07:06 -0600

    toggle term fix

--- dotfiles/nvim/lua/plugins/toggleterm.lua
+++ dotfiles/nvim/lua/plugins/toggleterm.lua
@@ -3,7 +3,7 @@ return {
 	config = function()
 		require("toggleterm").setup({
 			size = 20,
-			open_mapping = [[-]],
+			open_mapping = [[<C-t>]],
 			hide_numbers = true,
 			shade_filetypes = {},
 			shade_terminals = true,
