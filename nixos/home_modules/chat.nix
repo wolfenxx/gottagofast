3845cfd7169ff7033e63f755d341a7405be62a83
Carlos Salazar <csalazar@hey.com>
2025-01-27 17:48:53 -0600

    fix zoom screenshare

--- nixos/home_modules/chat.nix
+++ nixos/home_modules/chat.nix
@@ -1,8 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, pkgs-stable, ... }:
+let
+  stable-packages = with pkgs-stable; [
+    zoom-us
+  ];
+in
 {
   home.packages = with pkgs; [
-    zoom-us
     slack
     webcord
-  ]; 
+  ] ++ stable-packages; 
 }
