5ffe3302c6b7f1771aa15046226f27a9f4d1c95c
Carlos Salazar <csalazar@hey.com>
2025-02-20 16:06:29 -0600

    harpoon updates

--- dotfiles/nvim/lua/plugins/harpoon.lua
+++ dotfiles/nvim/lua/plugins/harpoon.lua
@@ -5,12 +5,17 @@ return {
 	config = function()
 		local harpoon = require("harpoon"):setup()
 
+		-- clear harpoon entries
+		vim.keymap.set("n", "<leader>hpc", function()
+			harpoon:list():clear()
+		end)
+
+		-- add new entry
 		vim.keymap.set("n", "<leader>a", function()
 			harpoon:list():add()
 		end)
-		-- commented out to use with telescope instead
-		--vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
+		-- select entries based on index
 		vim.keymap.set("n", "<C-y>", function()
 			harpoon:list():select(1)
 		end)
@@ -23,18 +28,42 @@ return {
 		vim.keymap.set("n", "<C-o>", function()
 			harpoon:list():select(4)
 		end)
+		vim.keymap.set("n", "<C-p>", function()
+			harpoon:list():select(5)
+		end)
 
+		-- remove entries based on index (note: removing at index 1 will remove all others. Possible bug)
 		vim.keymap.set("n", "<leader>yy", function()
-			harpoon:list():removeAt(1)
+			harpoon:list():remove_at(1)
 		end)
 		vim.keymap.set("n", "<leader>uu", function()
-			harpoon:list():removeAt(2)
+			harpoon:list():remove_at(2)
 		end)
 		vim.keymap.set("n", "<leader>ii", function()
-			harpoon:list():removeAt(3)
+			harpoon:list():remove_at(3)
 		end)
 		vim.keymap.set("n", "<leader>oo", function()
-			harpoon:list():removeAt(4)
+			harpoon:list():remove_at(4)
+		end)
+		vim.keymap.set("n", "<leader>pp", function()
+			harpoon:list():remove_at(5)
+		end)
+
+		-- replace entries based on index
+		vim.keymap.set("n", "<leader>ry", function()
+			harpoon:list():replace_at(1)
+		end)
+		vim.keymap.set("n", "<leader>ru", function()
+			harpoon:list():replace_at(2)
+		end)
+		vim.keymap.set("n", "<leader>ri", function()
+			harpoon:list():replace_at(3)
+		end)
+		vim.keymap.set("n", "<leader>ro", function()
+			harpoon:list():replace_at(4)
+		end)
+		vim.keymap.set("n", "<leader>rp", function()
+			harpoon:list():replace_at(5)
 		end)
 
 		-- Toggle previous & next buffers stored within Harpoon list
@@ -54,15 +83,15 @@ return {
 			end
 
 			require("telescope.pickers")
-				.new({}, {
-					prompt_title = "Harpoon",
-					finder = require("telescope.finders").new_table({
-						results = file_paths,
-					}),
-					previewer = conf.file_previewer({}),
-					sorter = conf.generic_sorter({}),
-				})
-				:find()
+					.new({}, {
+						prompt_title = "Harpoon",
+						finder = require("telescope.finders").new_table({
+							results = file_paths,
+						}),
+						previewer = conf.file_previewer({}),
+						sorter = conf.generic_sorter({}),
+					})
+					:find()
 		end
 
 		vim.keymap.set("n", "<C-e>", function()
