0f0540232b9e431b253cd50b78b556798ff3615b
Carlos Salazar <csalazar@hey.com>
2025-11-05 10:33:37 -0600

    add usbimager package

--- nixos/home_modules/home.nix
+++ nixos/home_modules/home.nix
@@ -1,19 +1,20 @@
 { pkgs, userSettings, ... }:
 {
   home.username = userSettings.username;
-  home.homeDirectory = "/home/"+userSettings.username;
+  home.homeDirectory = "/home/" + userSettings.username;
 
   home.stateVersion = "24.05";
   programs.home-manager.enable = true;
 
   home.packages = with pkgs; [
-     protonup
-     obsidian
-     pcsx2
-     mesen
-     snes9x
-     dolphin-emu
-		 #torzu # fork of yuzu (removed in latest version)
-     ledger-live-desktop
-  ]; 
+    protonup
+    obsidian
+    pcsx2
+    mesen
+    snes9x
+    dolphin-emu
+    #torzu # fork of yuzu (removed in latest version)
+    ledger-live-desktop
+    usbimager
+  ];
 }
